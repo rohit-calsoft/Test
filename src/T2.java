799b095b7581845e60f97a67b7b0950494843b44
rohit <rohit.chinchkar17@gmail.com>
2019-10-22 15:48:01 +0530

    initial

---
+++ src/T2.java
@@ -0,0 +1,13 @@
+import java.io.ObjectInputStream.GetField;
+
+public class T2 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("This is T2");
+
+	}
+
+}
